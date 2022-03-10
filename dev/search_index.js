21bf4f672caa433beeddaccfed69477451d500f6
Documenter.jl <documenter@juliadocs.github.io>
2022-03-10 02:07:51 +0000

    build based on bab9f7a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Dynesty","category":"page"},{"location":"#Dynesty","page":"Home","title":"Dynesty","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Dynesty.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Dynesty]","category":"page"}]
+}
